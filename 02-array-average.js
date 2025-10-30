cc5f8e77e6d0400f3d8340d53a6a478c49da08a7
Simeon Kazadi <simeonkazad@gmail.com>
2025-10-30 17:08:31 +0100

    resolution exercice 2

--- 02-array-average.js
+++ 02-array-average.js
@@ -3,9 +3,26 @@
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
+const notes= [12,24,8,10,25]
+
+
 function averageNote(notes) {
+
+	let sommes= 0;
+	
+	for (let i=0; i<notes.length; i++) {
+		sommes += notes[i];
+	}
+	const moyenne = sommes/notes.length;
+	if (moyenne>= 10) {
+		return "Reussi";
+	} else{
+		return "Échoué"
+	}
 	
 }
+const resultat = averageNote(notes);
+console.log (resultat)
 
 module.exports = {
 	averageNote,
